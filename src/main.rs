bddaea2dca1e033ee8e74ff64ff30ed9f7e8fc58
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-05-05 14:36:53 +0300

    search substring in file feature added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world! this is test program");
+    let args: Vec<String> = env::args().collect(); // get command line args
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing command line arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
