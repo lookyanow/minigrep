cdd6180e37047504dcfb3a1d8011a385c4d43d14
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-05-05 15:01:36 +0300

    env varible usage in config added

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // get command line args
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing command line arguments: {}", err);
+        eprintln!("Problem parsing command line arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
